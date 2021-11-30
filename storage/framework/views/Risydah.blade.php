36818341d5f59a53953ec9a1c69a539626125c44
VAIO-SVF14\VAIO <60900119049@uin-alauddin.ac.id>
2021-11-30 11:50:01 +0800

    asa

--- storage/framework/views/Risydah.blade.php
+++ storage/framework/views/Risydah.blade.php
@@ -1,3 +1,3 @@
  1 . Version control adalah sebuah sistem yang merekam perubahan-perubahan dari sebuah berkas atau sekumpulan berkas dari waktu ke waktu sehingga Anda dapat menilik kembali versi khusus suatu saat nanti.
  contohnya
- GIT, CVS, SVN, Mercurial, Bazaarr
+ GIT, CVS, SVN, Mercurial, Bazaarrsa
